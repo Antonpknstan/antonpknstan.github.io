d441f6f24445a496245c71d903d83695638b7510
Antonpknstan <126149270+Antonpknstan@users.noreply.github.com>
2023-07-19 21:33:20 +0700

    Update api.php

--- php/api.php
+++ php/api.php
@@ -2,9 +2,9 @@
 ini_set("display_errors", 0);
 
 // Set up API key, URL, and model
-$API_KEY = '';
+$API_KEY = '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';
 $url = 'https://api.openai.com/v1/completions';
-$model = "text-davinci-003";
+$model = "gpt-3.5-turbo";
 $chat = "";
 
 // Read input data
